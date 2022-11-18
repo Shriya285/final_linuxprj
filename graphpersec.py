110078600a2f4c2a92e70b40f0ccd50f99f11bfa
Shriya285 <shriyakonduru@gmail.com>
2022-11-18 23:23:34 +0530

    done1

--- graphpersec.py
+++ graphpersec.py
@@ -13,3 +13,26 @@ st.write("""
 
 df = pd.read_csv("data3.csv")
 st.line_chart(df)
+
+st.subheader("Persec")
+if st.checkbox("Show information"):
+    st.write("persec")
+
+df = pd.read_csv("persec.csv")
+fig = ff.create distplot(df)
+st.plotly_chart(fig)
+
+st.area_chart(df)
+df = pd.read_csv("step3.csv")
+df.hist()
+st.pyplot()
+#i am not quite sure but ig i have to put line 20 here.
+
+df = pd.read_csv("data2.csv")
+st.bar_chart(df)
+
+
+
+
+
+
